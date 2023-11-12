f081af4632705beecbb42817a663688cb0a6fb32
Billy <billy.xu99@gmail.com>
2023-11-12 20:53:11 +1000

    Declaring variables needed for game. Color and speeds.

---
+++ main.js
@@ -0,0 +1,16 @@
+const gameBoard = document.querySelector("#game-board")
+const context = gameBoard.getContext("2d")
+const scoreText = document.querySelector("#score-text")
+const resetButton = document.querySelector("#reset-button")
+const gameWidth = gameBoard.width
+const gameHeight = gameBoard.height
+const boardBackground = "silver"
+const paddle1Color = "lightblue"
+const paddle2Color = "red"
+const paddleBorder = "black"
+const ballColor = "black"
+const ballBorderColor = "green"
+const ballRadius = 12.5
+const paddleSpeed = 50
+let intervalID 
+let ballSpeed = 1;
