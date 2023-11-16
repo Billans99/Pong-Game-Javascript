05b72cb4fe1ca33efbcec089ca42b1e9aece3191
Billy <billy.xu99@gmail.com>
2023-11-16 17:57:53 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,84 +1,88 @@
-const gameBoard = document.querySelector("#game-board")
-const context = gameBoard.getContext("2d")
-const scoreText = document.querySelector("#score-text")
-const resetButton = document.querySelector("#reset-button")
-const gameWidth = gameBoard.width
-const gameHeight = gameBoard.height
-const boardBackground = "silver"
-const paddle1Color = "lightblue"
-const paddle2Color = "red"
-const paddleBorder = "black"
-const ballColor = "black"
-const ballBorderColor = "green"
-const ballRadius = 12.5
-const paddleSpeed = 50
-let intervalID 
-let ballSpeed = 1
-let ballX = gameWidth / 2
-let ballY = gameHeight / 2
-let ballXDirection = 0
-let ballYDirection = 0
-let player1Score = 0
-let player2Score = 0
+const gameBoard = document.querySelector("#game-board");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#score-text");
+const resetBtn = document.querySelector("#reset-button");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "forestgreen";
+const paddle1Color = "lightblue";
+const paddle2Color = "red";
+const paddleBorder = "black";
+const ballColor = "yellow";
+const ballBorderColor = "black";
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth / 2;
+let ballY = gameHeight / 2;
+let ballXDirection = 0;
+let ballYDirection = 0;
+let player1Score = 0;
+let player2Score = 0;
 let paddle1 = {
     width: 25,
     height: 100,
     x: 0,
     y: 0
-}
+};
 let paddle2 = {
     width: 25,
     height: 100,
     x: gameWidth - 25,
     y: gameHeight - 100
-}
+};
 
-window.addEventListener("keydown", changeDirection)
-resetButton.addEventListener("click", resetGame)
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
 
-gameStart()
+gameStart();
+drawPaddles();
 
 function gameStart() {
-
+    createBall();
+    nextTick();
 }
 
 function nextTick() {
-
-}
+    intervalID = setTimeOut(() => {
+        clearBoard();
+        drawPaddles();
+        moveBall();
+        drawBall(ballX, ballY);
+        checkCollision();
+        nextTick();
+    }, 10)
+};
 
 function clearBoard() {
-
-}
+    ctx.fillStyle = boardBackground
+    ctx.fillRect(0, 0, gameWidth, gameHeight)
+};
 
 function drawPaddles() {
+    ctx.strokeStyle = paddleBorder;
 
-}
+    ctx.fillStyle = paddle1Color;
+    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
 
-function createBall() {
+    ctx.fillStyle = paddle1Color
+    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+};
 
-}
+function createBall() {};
 
-function moveBall() {
+function moveBall() {};
 
-}
+function drawBall(ballX, ballY) {};
 
-function drawBall() {
+function checkCollision() {};
 
-}
+function changeDirection() {};
 
-function checkCollision() {
+function updateScore() {};
 
-}
-
-function changeDirection() {
-
-}
-
-function updateScore() {
-
-}
-
-function resetGame() {
-
-}
+function resetGame() {};
 
